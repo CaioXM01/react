3ec7b627cce41f476b9c93bae5bb043ef678c9de
CaioXM01 <caioxm01@gmail.com>
2019-11-11 01:58:47 -0300

    Importando imagens

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'; 
 import './App.css';
 
+import profile from './assets/0.jpeg';
+
 function App() {
-  return <h1>Hello Caio</h1>
+  return <img width="200" src={profile} />
 }
 
 export default App;
