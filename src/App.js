8115d4c1d293c1158f9270fb952e31179d902f4d
CaioXM01 <caioxm01@gmail.com>
2019-11-11 02:14:44 -0300

    Class Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'; 
 import './App.css';
 
-import profile from './assets/0.jpeg';
+import TechList from './components/TechList';
 
 function App() {
-  return <img width="200" src={profile} />
+  return <TechList />
 }
 
 export default App;
